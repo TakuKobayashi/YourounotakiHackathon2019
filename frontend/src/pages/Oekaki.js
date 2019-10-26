1d817c5e1be13cb688f46fb7b1a34eedab675a6d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-10-26 19:44:56 +0900

    ノートっぽい画像を読みこんで表示させた

--- frontend/src/pages/Oekaki.js
+++ frontend/src/pages/Oekaki.js
@@ -1,7 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PageImage from './pages.png'
+import { Application, Sprite } from 'pixi.js';
 
-export class Oekaki extends Component {
+export class Oekaki extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  onCanvasLoaded = (canvas) => {
+    this.pixiApp = new Application({view: canvas});
+    this.loadDefaultImage();
+  }
+
+  // すでに登録されている色紙の画像を読み込む
+  loadDefaultImage(){
+    const pageBg = Sprite.from(PageImage);
+    this.pixiApp.stage.addChild(pageBg);
+  }
+
   render() {
-    return <div>oekaki</div>;
+    return (
+      <div>
+        <canvas
+          style={{ width: "100%", height: "100%" }}
+          ref={this.onCanvasLoaded}
+        />
+      </div>
+    )
   }
 }
