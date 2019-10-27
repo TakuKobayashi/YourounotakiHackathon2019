232ae4c0af2d0a4010e451c4406e8db94f6179eb
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-10-27 09:10:41 +0900

    多分コレでお絵描きいいと思うんだけどな...

--- frontend/src/pages/Oekaki.js
+++ frontend/src/pages/Oekaki.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageImage from './pages.png'
-import { Application, Sprite, Texture } from 'pixi.js';
+import { Application, Sprite, Texture, Graphics, RenderTexture } from 'pixi.js';
 
 export class Oekaki extends React.Component {
   constructor(props){
@@ -14,55 +14,39 @@ export class Oekaki extends React.Component {
   }
 
   drawPath(){
-    const texture = Texture.from("https://pixijs.io/examples/examples/assets/bunny.png");
-    const bunny = new Sprite(texture);
+    const brush = new Graphics();
+    brush.beginFill(0x000000);
+    brush.drawCircle(0, 0, 50);
+    brush.endFill();
 
-    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-    bunny.interactive = true;
+    const renderTexture = RenderTexture.create(this.pixiApp.screen.width, this.pixiApp.screen.height);
 
-    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-    bunny.buttonMode = true;
+    const renderTextureSprite = new Sprite(renderTexture);
+    this.pixiApp.stage.addChild(renderTextureSprite);
 
-    // center the bunny's anchor point
-    bunny.anchor.set(0.5);
+    let dragging = false;
 
-    // make it a bit bigger, so it's easier to grab
-    bunny.scale.set(3);
-
-    // setup events for mouse + touch using
-    // the pointer events
-    bunny
-        .on('pointerdown', this.onDragStart)
-        .on('pointerup', this.onDragEnd)
-        .on('pointerupoutside', this.onDragEnd)
-        .on('pointermove', this.onDragMove);
-
-    // add it to the stage
-    this.pixiApp.stage.addChild(bunny);
-  }
-
-  onDragStart(event) {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.data = event.data;
-    this.alpha = 0.5;
-    this.dragging = true;
-  }
-
-  onDragEnd() {
-    this.alpha = 1;
-    this.dragging = false;
-    // set the interaction data to null
-    this.data = null;
-  }
-
-  onDragMove() {
-    if (this.dragging) {
-      const newPosition = this.data.getLocalPosition(this.parent);
-      this.x = newPosition.x;
-      this.y = newPosition.y;
+    const onDragEnd = function(event) {
+      dragging = false;
     }
+
+    const onDragMove = function(event) {
+      if (dragging) {
+        brush.position.copyFrom(event.data.global);
+        this.pixiApp.renderer.render(brush, renderTexture, false, null, false);
+      }
+    }
+
+    const onDragStart = function(event) {
+      console.log(event);
+      dragging = true;
+      onDragMove(event);
+    }
+
+    this.pixiApp.stage.on('pointerdown', onDragStart);
+    this.pixiApp.stage.on('pointerup', onDragEnd);
+    this.pixiApp.stage.on('pointerupoutside', onDragEnd)
+    this.pixiApp.stage.on('pointermove', onDragMove);
   }
 
   // すでに登録されている色紙の画像を読み込む
