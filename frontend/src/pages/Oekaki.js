4c078a2eab9d6c49c0cb991a54860fe0f3d414c2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-10-27 11:57:26 +0900

    いい感じにお絵描きできるようにした

--- frontend/src/pages/Oekaki.js
+++ frontend/src/pages/Oekaki.js
@@ -5,6 +5,10 @@ import { Application, Sprite, Texture, Graphics, RenderTexture } from 'pixi.js';
 export class Oekaki extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      lineColor: 0x000000,
+      strokeRange: 3,
+    }
   }
 
   onCanvasLoaded = (canvas) => {
@@ -14,52 +18,67 @@ export class Oekaki extends React.Component {
   }
 
   drawPath(){
-    const brush = new Graphics();
-    brush.beginFill(0x000000);
-    brush.drawCircle(0, 0, 50);
-    brush.endFill();
+    const app = this.pixiApp;
+    const self = this;
 
-    const renderTexture = RenderTexture.create(this.pixiApp.screen.width, this.pixiApp.screen.height);
+    const renderTexture = RenderTexture.create(app.stage.width, app.stage.height);
 
     const renderTextureSprite = new Sprite(renderTexture);
-    this.pixiApp.stage.addChild(renderTextureSprite);
+    app.stage.addChild(renderTextureSprite);
 
     let dragging = false;
+    let prevPosition = null;
 
     const onDragEnd = function(event) {
       dragging = false;
+      prevPosition = null;
     }
 
     const onDragMove = function(event) {
       if (dragging) {
-        brush.position.copyFrom(event.data.global);
-        this.pixiApp.renderer.render(brush, renderTexture, false, null, false);
+        const currentPosition = event.data.global.clone();
+        if(prevPosition){
+          const graphicPath = self.drawBezier(prevPosition, currentPosition);
+          app.renderer.render(graphicPath, renderTexture, false, null, false);
+        }
+        prevPosition = currentPosition;
       }
     }
 
     const onDragStart = function(event) {
-      console.log(event);
       dragging = true;
-      onDragMove(event);
+      prevPosition = event.data.global.clone();
     }
 
-    this.pixiApp.stage.on('pointerdown', onDragStart);
-    this.pixiApp.stage.on('pointerup', onDragEnd);
-    this.pixiApp.stage.on('pointerupoutside', onDragEnd)
-    this.pixiApp.stage.on('pointermove', onDragMove);
+    app.stage.interactive = true;
+    app.stage.on('pointerdown', onDragStart);
+    app.stage.on('pointerup', onDragEnd);
+    app.stage.on('pointerupoutside', onDragEnd)
+    app.stage.on('pointermove', onDragMove);
+  }
+
+  drawBezier(prevPosition, currentPosition){
+    const bezier = new Graphics();
+    bezier.lineStyle(this.state.strokeRange, this.state.lineColor, 1);
+    bezier.moveTo(prevPosition.x, prevPosition.y).lineTo(currentPosition.x,currentPosition.y);
+
+//    bezier.bezierCurveTo(prevPosition.x, prevPosition.y, (prevPosition.x + currentPosition.x) / 2 , (prevPosition.y + currentPosition.y) / 2, currentPosition.x, currentPosition.y);
+    return bezier;
   }
 
   // すでに登録されている色紙の画像を読み込む
   loadDefaultImage(){
     const pageBg = Sprite.from(PageImage);
+    this.pixiApp.stage.width = pageBg.width;
+    this.pixiApp.stage.height = pageBg.height;
     this.pixiApp.stage.addChild(pageBg);
+    this.pixiApp.renderer.resize(pageBg.width, pageBg.height)
   }
 
   render() {
     return (
       <div>
         <canvas
-          style={{ width: "100%", height: "100%" }}
           ref={this.onCanvasLoaded}
         />
       </div>
